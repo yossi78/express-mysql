899ab32ef1ed0e0f0640f414b8ba3ec7b67b5c4f
Yossi Tal <yossi78@gmail.com>
2023-07-18 10:51:11 +0300

    add create table

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const pgp = require('pg-promise')();
 
 
-
 const connection = {
   host: '127.0.0.1',
   port: 5432,
@@ -9,10 +8,26 @@ const connection = {
   user: 'postgres',
   password: '1234567'
 };
-
-
 const db = pgp(connection);
-
 module.exports = db;
+
+
+
+async function createUsersTable() {
+  try {
+    await db.none(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        email TEXT
+      )
+    `);
+    console.log('Users table created successfully');
+  } catch (error) {
+    console.error('Error creating users table:', error);
+  }
+}
+createUsersTable();
+
 
 
