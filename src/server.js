04f61e6a5a20108652c4fd689e259f594e81eeb5
Yossi Tal <yossi78@gmail.com>
2023-07-18 16:19:10 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
-const usersRoutes = require('./router/userRoute');
+const usersRoutes = require('./router/user.route');
 require('./db/sql-queries');
 
 
