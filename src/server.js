b9d0f204e6c91cef89d4e26cd4064ac3ba2bbde5
Yossi Tal <yossi78@gmail.com>
2023-07-18 16:07:28 +0300

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 const usersRoutes = require('./router/userRoute');
-const db = require('./db/db-connections');
 require('./db/sql-queries');
 
 
@@ -11,10 +10,7 @@ require('./db/sql-queries');
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-
 app.use(express.json()); // parse JSON requests
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -27,52 +23,6 @@ app.use('/users', usersRoutes);
 
 
 
-
-
-
-// app.get('/api/users', async (req, res) => {
-//   try {
-//     const users = await db.any('SELECT * FROM users');
-//     res.json(users);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// app.post('/api/users', async (req, res) => {
-//   try {
-//     const { name, email } = req.body;
-//     const user = await db.one('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
-//     res.json(user);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// app.put('/api/users/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const { name, email } = req.body;
-//     const user = await db.one('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name, email, id]);
-//     res.json(user);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-// app.delete('/api/users/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const user = await db.one('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
-//     res.json(user);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 
 
